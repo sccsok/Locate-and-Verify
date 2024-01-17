63ec1f2cee0410dadabc4acf16fb1fc1e7df74ab
sccsok <50013426+sccsok@users.noreply.github.com>
2024-01-17 13:37:47 +0800

    Update xception.py

--- model/xception.py
+++ model/xception.py
@@ -322,7 +322,7 @@ class TransferModel(nn.Module):
 
             def return_pytorch04_xception(pretrained=False):
                 # Raises warning "src not broadcastable to dst" but thats fine
-                model = xception(pretrained=False)
+                model = xception()
                 if pretrained:
                     # Load model in torch 0.4+
                     model.fc = model.last_linear
